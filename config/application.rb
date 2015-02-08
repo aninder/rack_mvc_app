d47598d8dcaf5b49724ab1a474aca2dfce173f44
aninder <aninder@gmail.com>
2015-02-08 10:08:26 +0530

    removed explicit loading of controllers

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,12 @@
 require 'rack_mvc'
 
+=begin
 $LOAD_PATH << File.join(File.dirname(__FILE__),"..","app","controllers")
-require "pages_controller"
+controllers = Dir.open(File.join(File.dirname(__FILE__),"..","app","controllers")).entries.select {|file| file.match(/rb$/)}
+controllers.each do |cont|
+ require cont
+end
+=end
 
 module Blog
   class Application < RackMvc::Application 
